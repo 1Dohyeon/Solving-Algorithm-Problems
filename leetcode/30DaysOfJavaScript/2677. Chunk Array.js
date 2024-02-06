bd08e9f740cc7aa20fb94c5315ab1e1d40d5e27c
1Dohyeon <dh1072005@gmail.com>
2024-02-06 15:40:51 +0900

    문제 다른 풀이 작성

--- leetcode/30DaysOfJavaScript/2677. Chunk Array.js
+++ leetcode/30DaysOfJavaScript/2677. Chunk Array.js
@@ -12,3 +12,26 @@ var chunk = function (arr, size) {
 
   return result;
 };
+
+var chunk2 = function (arr, size) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arrSlice(arr, i, i + size));
+  }
+
+  return result;
+};
+
+const arrSlice = (arr, start, end) => {
+  let output = [];
+  for (let i = start; i < end; i++) {
+    if (arr[i] != null) output.push(arr[i]);
+  }
+  return output;
+};
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newArr = arrSlice(arr, 3, 7);
+console.log(newArr);
